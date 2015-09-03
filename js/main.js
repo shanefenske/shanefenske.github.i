112edd59556716be90cc544c69e163dcc8b50268
Shane Fenske <shanefenske1@gmail.com>
2015-09-02 20:52:47 -0400

    disable skrollr on mobile

--- js/main.js
+++ js/main.js
@@ -37,31 +37,84 @@ $(document).ready(function () {
 		$body.removeClass('loading').addClass('loaded');}
 });
 
-	function adjustWindow(){
+// 	function adjustWindow(){
 		
 
-		// Init Skrollr
-var s = skrollr.init({
-    forceHeight: false
-});
+// 		// Init Skrollr
+// var s = skrollr.init({
+//     forceHeight: false
+// });
  
 
 
 		
-		// Get window size
-	    winH = $window.height();
+// 		// Get window size
+// 	    winH = $window.height();
 	    
-	    // Keep minimum height 550
-	    if(winH <= 550) {
-			winH = 550;
-		} 
+// 	    // Keep minimum height 550
+// 	    if(winH <= 550) {
+// 			winH = 550;
+// 		} 
 	    
-	    // Resize our slides
-	    $slide.height(winH);
+// 	    // Resize our slides
+// 	    $slide.height(winH);
 	    
-	    // Refresh Skrollr after resizing our sections	    
-	    s.refresh($('.homeSlide'));	    
-	}
+// 	    // Refresh Skrollr after resizing our sections	    
+// 	    s.refresh($('.homeSlide'));	    
+// 	}
+function adjustWindow(){
+     
+    // Get window size
+    winH = $window.height();
+    winW = $window.width();
+     
+    // Keep minimum height 550
+    if(winH <= 550) {
+        winH = 550;
+    }
+     
+    // Init Skrollr for 768 and up
+    if( winW >= 768) {
+ 
+        // Init Skrollr
+        var s = skrollr.init({
+            forceHeight: false
+        });
+ 
+        // Resize our slides
+        $slide.height(winH);
+ 
+        s.refresh($('.homeSlide'));
+ 
+    } else {
+ 
+        // Init Skrollr
+        var s = skrollr.init();
+        s.destroy();
+    }
+ 
+    // Check for touch
+    if(Modernizr.touch) {
+ 
+        // Init Skrollr
+        var s = skrollr.init();
+        s.destroy();
+    }
+ 
+}
+
+function initAdjustWindow() {
+    return {
+        match : function() {
+            adjustWindow();
+        },
+        unmatch : function() {
+            adjustWindow();
+        }
+    };
+}
+ 
+enquire.register("screen and (min-width : 768px)", initAdjustWindow(), false);
 		
 } )( jQuery );
 
